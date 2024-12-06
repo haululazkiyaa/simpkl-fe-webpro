744a56ec0d323355a0d1d60812f0555bab58632c
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-12-06 23:03:32 +0700

    fix monthly page error

--- src/pages/student/final-grade/index.jsx
+++ src/pages/student/final-grade/index.jsx
@@ -1,17 +1,8 @@
-import OnDev from "../../../components/Elements/EmptyState/OnDev";
-
 export default function StudentFinalGradePage() {
+  // logic disini
   return (
     <>
-      <div className="format max-w-none mb-5">
-        {/* <h1 className="dark:text-white">Penilaian Akhir</h1> */}
-        <div className="flex flex-col justify-center items-center py-20">
-          <OnDev />
-          <h3 className="text-xl text-black font-bold mb-5 dark:text-white">
-            Fitur belum tersedia untuk saat ini.
-          </h3>
-        </div>
-      </div>
+      <p>tampilan disini</p>
     </>
   );
 }
--- src/pages/student/monthly-grade/index.jsx
+++ src/pages/student/monthly-grade/index.jsx
@@ -1,79 +1,15 @@
-import { useCallback, useContext, useEffect, useState } from "react";
-
-import { AuthContext } from "../../../context/AuthContext";
-import Logout from "../../../components/Elements/Logout";
-import StudentMonthlyGradeTableView from "../../../views/Student/MonthlyGrade/TableView";
-import StudentMonthlyGradeUpdateDrawerView from "../../../views/Student/MonthlyGrade/UpdateDrawerView";
-import StudentMonthlyGradeView from "../../../views/Student/MonthlyGrade/GradeView";
-import { getNilaiBulananSiswa } from "../../../services/student/monthly-grade.service";
-import { refreshToken } from "../../../services/auth/auth.service";
-import { useNavigate } from "react-router-dom";
+import OnDev from "../../../components/Elements/EmptyState/OnDev";
 
 export default function StudentMonthlyGradePage() {
-  const { setProgress } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const [data, setData] = useState([]);
-  const [selected, setSelected] = useState("");
-  const [bulan, setBulan] = useState("");
-  const [tahun, setTahun] = useState("");
-
-  const handleDataHarian = useCallback(() => {
-    setData([]);
-    if (bulan === "" && tahun === "") {
-      const today = new Date();
-      setBulan(today.getMonth() + 1);
-      setTahun(today.getFullYear());
-    }
-
-    setProgress(30);
-    refreshToken((status, token) => {
-      if (status) {
-        setProgress(60);
-        getNilaiBulananSiswa(bulan, tahun, token, (status, data) => {
-          if (status) {
-            setData(data);
-          }
-        });
-      } else {
-        Logout((status) => {
-          if (status) {
-            navigate("/login");
-          }
-        });
-      }
-      setProgress(100);
-    });
-  }, [setProgress, navigate, bulan, tahun]);
-
-  useEffect(() => {
-    handleDataHarian();
-  }, [handleDataHarian]);
-
   return (
     <>
       <div className="format max-w-none mb-5">
-        <h1 className="dark:text-white">Penilaian Bulanan</h1>
-        <p>
-          Anda dapat melihat nilai bulanan yang sudah diberikan oleh Guru
-          Pembimbing Anda di akhir bulan. Jika data nilai belum tersedia,
-          hubungi Guru Pembimbing Anda untuk memastikannya.
-        </p>
-        <div className="not-format">
-          <StudentMonthlyGradeTableView
-            data={data}
-            setSelected={setSelected}
-            bulan={bulan}
-            setBulan={setBulan}
-            tahun={tahun}
-            setTahun={setTahun}
-          />
-          <StudentMonthlyGradeUpdateDrawerView
-            handleDataHarian={handleDataHarian}
-            selected={selected}
-            id="1"
-          />
-          <StudentMonthlyGradeView data={data} selected={selected} />
+        {/* <h1 className="dark:text-white">Jurnal Bulanan</h1> */}
+        <div className="flex flex-col justify-center items-center py-20">
+          <OnDev />
+          <h3 className="text-xl text-black font-bold mb-5 dark:text-white">
+            Fitur belum tersedia untuk saat ini.
+          </h3>
         </div>
       </div>
     </>
